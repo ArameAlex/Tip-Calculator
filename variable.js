3fcbf67d654e10a669d99411c9e2298798ae9ae6
ArameAlex <alexanderianarame@gmail.com>
2024-12-25 15:37:14 +0330

    dark-light_mode

--- variable.js
+++ variable.js
@@ -11,3 +11,26 @@ function calculateTotal() {
 }
 
 btnEl.addEventListener("click", calculateTotal);
+
+const toggleButton = document.getElementById("mode-toggle");
+const body = document.body;
+const div = document.getElementById("box");
+
+function ligtOrDark(){
+  // if it was dark mode
+  if (body.classList.contains("dark-mode")){
+    // button color and text
+    toggleButton.textContent = "Switch to Dark Mode";
+    toggleButton.classList = "dark-mode";
+    // body color
+    body.classList = "light-mode";
+    div.classList = "light-mode main_box";
+  } else if (body.classList.contains("light-mode")){
+    // button color and text
+    toggleButton.textContent = "Switch to Light Mode";
+    toggleButton.classList = "light-mode";
+    // body color
+    body.classList = "dark-mode";
+    div.classList = "dark-mode main_box";
+  }
+}
